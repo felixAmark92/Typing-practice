1ea36a42ea2968babb219dc26372da3008ac31a6
felixAmark92 <felix.amark@gmail.com>
2024-05-07 09:59:18 +0200

    changes

--- server/main.js
+++ server/main.js
@@ -21,7 +21,11 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+    "https://thankful-ground-031ea5703.5.azurestaticapps.net/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
